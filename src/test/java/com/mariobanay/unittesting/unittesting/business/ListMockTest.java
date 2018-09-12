47be5c6ab259909c373c33b5e80497f063faff66
Mario Banay <banay.mario@gmail.com>
2018-09-12 21:49:14 +0200

    Added spy test

--- src/test/java/com/mariobanay/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/mariobanay/unittesting/unittesting/business/ListMockTest.java
@@ -6,10 +6,12 @@ import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -93,8 +95,30 @@ public class ListMockTest {
 		// želimo provjeriti koji je argument prosljeđen add
 		assertEquals("SomeString1", captor.getAllValues().get(0));
 		assertEquals("SomeString2", captor.getAllValues().get(1));
-		
-		
 	}
 	
+	@Test
+	public void mocking() {
+		ArrayList arrayListMock = mock(ArrayList.class);
+		System.out.println(arrayListMock.get(0));
+		System.out.println(arrayListMock.size());
+		arrayListMock.add("Test");
+		arrayListMock.add("Test2");
+		System.out.println(arrayListMock.size());
+		when(arrayListMock.size()).thenReturn(5);
+		System.out.println(arrayListMock.size());
+	}
+	
+	@Test
+	public void spying() {
+		ArrayList arrayListSpy = spy(ArrayList.class);
+		arrayListSpy.add("Test0");
+		System.out.println(arrayListSpy.get(0));
+		System.out.println(arrayListSpy.size());
+		arrayListSpy.add("Test");
+		arrayListSpy.add("Test2");
+		System.out.println(arrayListSpy.size());
+		when(arrayListSpy.size()).thenReturn(5);
+		System.out.println(arrayListSpy.size());
+	}
 }
