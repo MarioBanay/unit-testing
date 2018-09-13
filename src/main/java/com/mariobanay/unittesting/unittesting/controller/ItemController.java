2831be0ea17293c6d14bfe2be5df903926c437db
Mario Banay <banay.mario@gmail.com>
2018-09-13 11:16:07 +0200

    Created ItemBusinessService Class

--- src/main/java/com/mariobanay/unittesting/unittesting/controller/ItemController.java
+++ src/main/java/com/mariobanay/unittesting/unittesting/controller/ItemController.java
@@ -1,15 +1,27 @@
 package com.mariobanay.unittesting.unittesting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mariobanay.unittesting.unittesting.business.ItemBusinessService;
 import com.mariobanay.unittesting.unittesting.model.Item;
 
 @RestController
 public class ItemController {
 	
+	@Autowired
+	private ItemBusinessService businesService;
+	
 	@GetMapping("/dummy-item")
 	public Item dummyItem() {
 		return new Item(1, "Ball", 10, 100);
 	}
+	
+	@GetMapping("/item-from-business-service")
+	public Item itemFromBusinesService() {
+		return businesService.retrieveHardcodedItem();
+	}
+	
+	
 }
