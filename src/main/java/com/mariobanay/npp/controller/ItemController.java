d62774c761dc7d834ce69af82976a1e58424f17a
Mario Banay <banay.mario@gmail.com>
2018-09-13 23:53:23 +0200

    Tested Aop Aspects

--- src/main/java/com/mariobanay/npp/controller/ItemController.java
+++ src/main/java/com/mariobanay/npp/controller/ItemController.java
@@ -1,10 +1,8 @@
 package com.mariobanay.npp.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
